5bc9687f47d59f4546dd883862d1d9df7734eeb9
Nithish kumar K <knithish432@gmail.com>
2024-07-30 01:30:15 +0530

    sb-admin

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { useState } from "react";
+import Dashboard from './components/Dashboard';
+import Sidebar from './components/Sidebar';
+
+function App() {
+
+  let data = {
+    earningsMonthly:"4,00,000",
+    earningsAnnual:"45,00,000",
+    task:"80",
+    pendingRequest:18,
+  }
+  
+  const [value,setValue] = useState("Hii")
+
+  return <>
+  <div id="wrapper">
+    <Sidebar/>
+    <h1>{value}</h1>
+    <Dashboard data={{data,setValue}}/>
+    
+  </div>
+  </>
+}
+
+export default App;
